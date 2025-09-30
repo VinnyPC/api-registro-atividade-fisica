143e7c92df44bfb9c9ecfd9d7c5d8384cf64a1e7
VinnyPC <vinisilvax3@gmail.com>
2025-09-29 22:01:36 -0300

    feat: implementado buscar todas sem filtros

--- app/services/atividade_service.py
+++ app/services/atividade_service.py
@@ -14,6 +14,10 @@ class AtividadeService:
     @staticmethod
     def listar_atividades(page=1, per_page=10, filters=None):
         return AtividadeRepository.get_paginated_and_filtered(page, per_page, filters)
+    
+    @staticmethod
+    def buscar_todas():
+        return AtividadeRepository.get_all()
 
     @staticmethod
     def buscar_por_funcional(funcional):
